b27b52fe8cc64f182902b48a562a5b724bf09417
Manish Nagdewani <nagdewanimanis@gmail.com>
2015-05-04 18:49:46 +0530

    comment added

--- railstest.rb
+++ railstest.rb
@@ -1,3 +1,4 @@
+#check rational no is able to divisable enough and check the sign of rational no too and return rational no 
 def check_no(c)
   if c.numerator % c.denominator ==0
     return c
@@ -13,17 +14,16 @@ def check_no(c)
   end 
     return q ? "#{q}"+" #{Rational(n,d)}" :  c
 end 
-a = gets.chomp
+a = gets.chomp ##take input as string
 rat_arr,is_neg = Array.new,false
-a.split.each {|d| rat_arr << Rational(d)}
-puts rat_arr
+a.split.each {|d| rat_arr << Rational(d)} #split string
 a,b = check_no(rat_arr[0]),check_no(rat_arr[1])
 puts "#{a} + #{b} ="
-c = Rational(rat_arr[0]) + Rational(rat_arr[1])
+c = Rational(rat_arr[0]) + Rational(rat_arr[1]) ##add nos
 puts check_no(c)
 puts "#{a} - #{b} ="
-c = Rational(rat_arr[0]) - Rational(rat_arr[1])
+c = Rational(rat_arr[0]) - Rational(rat_arr[1]) ##subs nos
 puts check_no(c)
 puts "#{a} * #{b} ="
-c = Rational(rat_arr[0]) * Rational(rat_arr[1])
+c = Rational(rat_arr[0]) * Rational(rat_arr[1]) ##muliply nos
 puts check_no(c)
