5237b2c465ca09f9f2a6a4fa72785e8ec8da834d
Manish Nagdewani <nagdewanimanis@gmail.com>
2015-05-04 17:34:03 +0530

    rational op

---
+++ railstest.rb
@@ -0,0 +1,29 @@
+def check_no(c)
+  if c.numerator % c.denominator ==0
+    return c
+  elsif c.numerator < 0
+    is_neg = true
+    c = 0-c 
+  end
+  if c.numerator > c.denominator
+    q = c.numerator / c.denominator
+    n = c.numerator - q * c.denominator 
+    q = is_neg ? -q : q
+    d = c.denominator
+  end 
+    return q ? "#{q}"+" #{Rational(n,d)}" :  c
+end 
+a = gets.chomp
+rat_arr,is_neg = Array.new,false
+a.split.each {|d| rat_arr << Rational(d)}
+puts rat_arr
+a,b = check_no(rat_arr[0]),check_no(rat_arr[1])
+puts "#{a} + #{b} ="
+c = Rational(rat_arr[0]) + Rational(rat_arr[1])
+puts check_no(c)
+puts "#{a} - #{b} ="
+c = Rational(rat_arr[0]) - Rational(rat_arr[1])
+puts check_no(c)
+puts "#{a} * #{b} ="
+c = Rational(rat_arr[0]) * Rational(rat_arr[1])
+puts check_no(c)
